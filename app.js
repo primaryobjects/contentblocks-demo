a1e15478febe0c117345b03b262b904d19840186
K <kbecker@primaryobjects.com>
2013-01-02 21:40:02 -0500

    Initial commit.

---
+++ app.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , app = express()
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , contentBlocks = require('contentblocks')({ app: app, host: 'red-ant.herokuapp.com', pathFind: '/v1/nest/find?q={"@subject":"[id]"}', pathPost: '/v1/nest', pathPut: '/v1/nest/[id]', pathDelete: '/v1/nest/[id]' });
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(contentBlocks.render);
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+  app.locals.pretty = true;
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ public/scripts/contentblocks.js
@@ -0,0 +1,133 @@
+//
+// Url to your REST CMS web service 'find' interface. This is the method that checks if a content block already exists.
+// The service should accept GET/POST/PUT/UPDATE/DELETE with a JSON object. [id] will be replaced by the CMS content block id.
+// Example: http://your.service.com/api/findById?id=[id]
+//
+var restUrl = 'http://red-ant.herokuapp.com/v1/nest/find?q={"@subject": "[id]"}';
+var apiKey = 'contentblocksdemo'; // Unique key for the demo REST web service, red-ant.herokuapp.com. If you use the demo service, change this key to be unique. If using your own service, you can leave this blank.
+
+$(document).ready(function () {
+    $('body').midgardCreate({
+        metadata: {
+            midgardTags: {}
+        },
+        collectionWidgets: {
+            'default': 'midgardCollectionAdd',
+            'skos:related': null
+        },
+		state: 'edit'
+    });
+
+    // Set a simpler editor for title fields
+    $('body').midgardCreate('configureEditor', 'title', 'halloWidget', {
+        plugins: {
+            halloformat: {},
+            halloblacklist: {
+                tags: ['br']
+            }
+        }
+    });
+
+    $('body').midgardCreate('setEditorForProperty', 'dcterms:title', 'title');
+
+    // Disable editing of author fields
+    $('body').midgardCreate('setEditorForProperty', 'dcterms:author', null);
+});
+
+// Backbone.sync
+Backbone.sync = function (method, model, options) {
+    // Include web service key.
+    CommonManager.addApiKey(model, apiKey);
+
+    if (method == 'create') {
+        $.ajax({
+            url: '/create',
+            type: 'POST',
+            data: CommonManager.encodeId(JSON.stringify(model)),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (data) {
+                options.success(model);
+            }
+        });
+    }
+    else if (method == 'read') {
+    }
+    else if (method == 'update') {
+        $.ajax({
+            url: restUrl.replace('[id]', CommonManager.encodeId(model.id)),
+            type: 'GET',
+            dataType: 'jsonp',
+            success: function (data) {
+                if (data.length == 0) {
+                    // Create new entry.	
+                    $.ajax({
+                        url: '/create',
+                        type: 'POST',
+                        data: CommonManager.encodeId(JSON.stringify(model)),
+                        contentType: 'application/json',
+                        dataType: 'json',
+                        success: function (data) {
+                            options.success(model);
+                        }
+                    });
+                }
+                else {
+                    // Update existing entry.
+                    $.ajax({
+                        url: '/create/' + data[0]._id,
+                        type: 'PUT',
+                        data: CommonManager.encodeId(JSON.stringify(model)),
+                        contentType: 'application/json',
+                        dataType: 'json',
+                        success: function (data) {
+                            options.success(model);
+                        }
+                    });
+                }
+            }
+        });
+    }
+    else if (method == 'delete') {
+        $.ajax({
+            url: '/create/' + data[0]._id,
+            type: 'DELETE',
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (data) {
+                options.success(model);
+            }
+        });
+    }
+};
+
+CommonManager = {
+    encodeId: function (id) {
+        id = id.replace(/\./g, '_p').replace(/\//g, '_s');
+
+        return id;
+    },
+
+    decodeId: function (id) {
+        id = id.replace(/_p/g, '.').replace(/_s/g, '/');
+
+        return id;
+    },
+
+    addApiKey: function (model, apikey) {
+        if (apiKey.length > 0) {
+            model['apiKey'] = apiKey;
+
+            // Copy original stringify method.
+            model.toJSONOld = model.toJSON;
+
+            // Override method to include apiKey.
+            model.toJSON = function () {
+                var j = model.toJSONOld();
+                j.apiKey = model.apiKey;
+
+                return j;
+            }
+        }
+    }
+};
